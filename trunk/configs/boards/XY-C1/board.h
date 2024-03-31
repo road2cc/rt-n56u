885db53beed912520e4e8149b001c7bf7a9e1c17
road2cc <165169645+road2cc@users.noreply.github.com>
2024-03-31 14:49:03 +0800

    Update board.h

--- trunk/configs/boards/XY-C1/board.h
+++ trunk/configs/boards/XY-C1/board.h
@@ -1,34 +1,38 @@
-/* XY-C1/C5 */
+/* XZB */
 
-#define BOARD_PID		"XY-C1"
-#define BOARD_NAME		"XY-C1"
-#define BOARD_DESC		"XY-C1 Router"
-#define BOARD_VENDOR_NAME	"ChengDu JiZhi"
-#define BOARD_VENDOR_URL	"https://www.xyblock.net/"
-#define BOARD_MODEL_URL		"https://www.xyblock.net/"
+#define BOARD_PID		  "XZB"
+#define BOARD_NAME		"XZB (TimeCloud)"
+#define BOARD_DESC		"Xunlei XZB (TimeCloud)"
+#define BOARD_VENDOR_NAME	"Xunlei(Shenzheng) Limited"
+#define BOARD_VENDOR_URL	"http://www.xunlei.com/"
+#define BOARD_MODEL_URL		"http://www.xunlei.com/"
 #define BOARD_BOOT_TIME		30
 #define BOARD_FLASH_TIME	120
 #undef BOARD_GPIO_BTN_FN1
-#define BOARD_GPIO_BTN_RESET	18
-#undef  BOARD_GPIO_BTN_WPS
+#define BOARD_GPIO_BTN_RESET	13
+#define BOARD_GPIO_BTN_WPS	4
+#define BOARD_GPIO_SWICH_SD	43
 #undef  BOARD_GPIO_LED_ALL
 #undef  BOARD_GPIO_LED_WIFI
 #undef  BOARD_GPIO_LED_SW2G
 #undef  BOARD_GPIO_LED_SW5G
-#define  BOARD_GPIO_LED_POWER	31 // NA
-#define  BOARD_GPIO_LED_LAN	7 //SYS
-#define BOARD_GPIO_LED_WAN	8 //WORK
-#undef BOARD_GPIO_LED_USB	
-#undef BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_LED_POWER	31 // NA
+#undef  BOARD_GPIO_LED_LAN	7 //SYS
+#undef BOARD_GPIO_LED_WAN	8 //WORK
+
+#define	 BOARD_GPIO_LED_USB	7
+#define  BOARD_GPIO_LED_ROUTER	8
+#define  BOARD_GPIO_PWR_USB	11
+
 #define BOARD_HAS_5G_11AC	0
-#define BOARD_NUM_ANT_5G_TX	2
-#define BOARD_NUM_ANT_5G_RX	2
-#define BOARD_NUM_ANT_2G_TX	2
-#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	0
+#define BOARD_NUM_ANT_2G_RX	0
 #define BOARD_NUM_ETH_LEDS	1
 #define BOARD_HAS_EPHY_L1000	1
 #define BOARD_HAS_EPHY_W1000	1
 #define BOARD_NUM_UPHY_USB3	1
-#define BOARD_GPIO_PWR_USB	11
+
 #define BOARD_GPIO_PWR_USB_ON	1
 #define BOARD_USB_PORT_SWAP	0
